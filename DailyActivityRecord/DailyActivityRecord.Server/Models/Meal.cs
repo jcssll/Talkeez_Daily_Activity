a42cb300415349ffd75d6f60f6f29291f0e71a82
jcssll <essentialnetworker@gmail.com>
2025-06-09 01:07:13 -0400

    Relocated Models
    Relocated Models to the Models folder

---
+++ DailyActivityRecord/DailyActivityRecord.Server/Models/Meal.cs
@@ -0,0 +1,9 @@
+﻿namespace DailyActivityRecord.Server
+{
+    public class Meal
+    {
+        public string Type { get; set; } // Snack, Lunch
+        public DateTime Time { get; set; }
+        public string Amount { get; set; } // All, Most, Some
+    }
+}
---
+++ DailyActivityRecord/DailyActivityRecord.Server/Models/PottyRecord.cs
@@ -0,0 +1,9 @@
+﻿namespace DailyActivityRecord.Server
+{
+    public class PottyRecord
+    {
+        public DateTime Time { get; set; }
+        public List<string> Status { get; set; } // Wet, Dirty, Dry, Potty
+
+    }
+}
