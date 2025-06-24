02e600ca50af2f358c6e4fa7cc44a194c4d3fbff
jcssll <essentialnetworker@gmail.com>
2025-06-24 10:48:50 -0400

    Create 20250624133954_RecreateChildUserAccess.cs
    Created from EF recreation of database table

---
+++ DailyActivityRecord/DailyActivityRecord.Server/Migrations/20250624133954_RecreateChildUserAccess.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DailyActivityRecord.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class RecreateChildUserAccess : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
